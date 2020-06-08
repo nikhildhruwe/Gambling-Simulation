88fc45829288740efce725ea9b6c0b4e1f15c4b0
unknown <nick.dhruwe@gmail.com>
2020-06-08 18:15:39 +0530

    added win or loose

--- Gambling.sh
+++ Gambling.sh
@@ -2,3 +2,14 @@
 
 stake=100
 betMoney=1
+win=1
+loose=0
+
+if (($((RANDOM%2))==$win))
+then
+	money=$(($stake + $betMoney))
+	echo "Won the bet"
+else
+	money=$(($stake - $betMoney))
+	echo "Lost the bet"
+fi
