521b99c212256a6d087fea92c83d76ed926e2676
unknown <nick.dhruwe@gmail.com>
2020-06-09 01:49:35 +0530

    lucky/unlucky

--- Gambling.sh
+++ Gambling.sh
@@ -6,6 +6,7 @@ loose=0
 stakePerDay=100
 maxDays=20
 
+
 for (( day=1;$day<=$maxDays;day++ ))
 do
 	stake=$stakePerDay
@@ -30,25 +31,67 @@ do
 	if (( $stake>$stakePerDay))
 	then
 		difference=$(($stake-$stakePerDay))
-		day[$day]=won
+		Day[$day]=won
 		echo "Day $day won by : $difference"
 	else
 		difference=$(($stakePerDay-$stake))
-		day[$day]=loose
+		Day[$day]=loose
 
 		echo "Day $day lost by : $difference"
 	fi
 done
 
+#echo ${Day[@]}
+
 count=1
+luckyTemp=0
+unluckyTemp=0
 while (($count<=$maxDays))
 do
-	flag=0
-	if (( day[$count]==won ))
+	####  finding Lucky Day
+	if [[ "${Day[$count]}" == "won" ]]
 	then
-		if ((flag))
-	else
-		unlucky++
+		luckyCount=0
+		for ((i=$count;$i<=$maxDays;i++))
+		do
+			if [[ "${Day[$i]}" == "won" ]]
+			then
+				((luckyCount++))
+			else
+				break
+			fi
+		done
+
+		if (( $luckyCount>$luckyTemp ))
+		then
+			luckyTemp=$luckyCount
+			luckyDay=$(($i-1))
+		fi
 	fi
+
+	#####  finding unlucky Day
+	if [[ "${Day[$count]}" == "loose" ]]
+   then
+      unluckyCount=0
+      for ((j=$count;$j<=$maxDays;j++))
+      do
+         if [[ "${Day[$j]}" == "loose" ]]
+         then
+            ((unluckyCount++))
+         else
+            break
+         fi
+      done
+
+      if (( $unluckyCount>$unluckyTemp ))
+      then
+         unluckyTemp=$unluckyCount
+         unluckyDay=$(($j-1))
+      fi
+   fi
+
 ((count++))
 done
+
+echo "most lucky day was  : $luckyDay"
+echo "most unlucky day was: $unluckyDay"
