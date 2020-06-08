71d4f4f2d07bf828d05e2cfa99e58d3624608c0f
unknown <nick.dhruwe@gmail.com>
2020-06-08 22:18:29 +0530

    amt win or lost in 20 days

--- Gambling.sh
+++ Gambling.sh
@@ -4,25 +4,36 @@ betMoney=1
 win=1
 loose=0
 stakePerDay=100
+maxDays=20
 
-
-stake=$stakePerDay
-while (( 1 ))
+for (( day=1;$day<=$maxDays;day++ ))
 do
-	if (($((RANDOM%2))==$win))
-	then
-		stake=$(($stake + $betMoney))
-		#echo "Won the bet"
-	else
-		stake=$(($stake - $betMoney))
-		#echo "Lost the bet"
-	fi
+	stake=$stakePerDay
+	while (( 1 ))
+	do
+		if (($((RANDOM%2))==$win))
+		then
+			stake=$(($stake + $betMoney))
+			#echo "Won the bet"
+		else
+			stake=$(($stake - $betMoney))
+			#echo "Lost the bet"
+		fi
 
-	if (( $stake==$(($stakePerDay*50/100)) || $stake==$(($stakePerDay +$stakePerDay*50/100)) ))
+		if (( $stake==$(($stakePerDay*50/100)) || $stake==$(($stakePerDay +$stakePerDay*50/100)) ))
+		then
+			echo "Resigning for the day"
+			break;
+		fi
+	done
+
+	if (( $stake>$stakePerDay))
 	then
-		echo "Resigning for the day"
-		break;
+		differnce=$(($stake-$stakePerDay))
+		echo "Total amount won: $differnce"
+	else
+		difference=$(($stakePerDay-$stake))
+		echo "Total amount lost: $differnce"
 	fi
 done
 
-echo "Money after Gambling for the day : $stake"
