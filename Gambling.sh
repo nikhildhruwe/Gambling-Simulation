7f397ac6943f712a1e3b0428438d8803785d2e16
unknown <nick.dhruwe@gmail.com>
2020-06-09 02:09:59 +0530

    continue for next month or not

--- Gambling.sh
+++ Gambling.sh
@@ -6,27 +6,28 @@ loose=0
 stakePerDay=100
 maxDays=20
 
-
-for (( day=1;$day<=$maxDays;day++ ))
+while ((1))
 do
-	stake=$stakePerDay
-	while (( 1 ))
+	for (( day=1;$day<=$maxDays;day++ ))
 	do
-		if (($((RANDOM%2))==$win))
-		then
-			stake=$(($stake + $betMoney))
-			#echo "Won the bet"
-		else
-			stake=$(($stake - $betMoney))
-			#echo "Lost the bet"
-		fi
+		stake=$stakePerDay
+		while (( 1 ))
+		do
+			if (($((RANDOM%2))==$win))
+			then
+				stake=$(($stake + $betMoney))
+				#echo "Won the bet"
+			else
+				stake=$(($stake - $betMoney))
+				#echo "Lost the bet"
+			fi
 
-		if (( $stake==$(($stakePerDay*50/100)) || $stake==$(($stakePerDay +$stakePerDay*50/100)) ))
-		then
-			#echo "Resigning for the day"
-			break;
-		fi
-	done
+			if (( $stake==$(($stakePerDay*50/100)) || $stake==$(($stakePerDay +$stakePerDay*50/100)) ))
+			then
+				#echo "Resigning for the day"
+				break;
+			fi
+		done
 
 	if (( $stake>$stakePerDay))
 	then
@@ -39,59 +40,76 @@ do
 
 		echo "Day $day lost by : $difference"
 	fi
-done
+		totalMoney=$(($totalMoney + $stake))
 
-#echo ${Day[@]}
+	done
 
-count=1
-luckyTemp=0
-unluckyTemp=0
-while (($count<=$maxDays))
-do
-	####  finding Lucky Day
-	if [[ "${Day[$count]}" == "won" ]]
-	then
-		luckyCount=0
-		for ((i=$count;$i<=$maxDays;i++))
-		do
-			if [[ "${Day[$i]}" == "won" ]]
-			then
-				((luckyCount++))
-			else
-				break
-			fi
-		done
+	#echo  total money =$totalMoney
 
-		if (( $luckyCount>$luckyTemp ))
+	count=1
+	luckyTemp=0
+	unluckyTemp=0
+	while (($count<=$maxDays))
+	do
+		####  finding Lucky Day
+		if [[ "${Day[$count]}" == "won" ]]
 		then
-			luckyTemp=$luckyCount
-			luckyDay=$(($i-1))
+			luckyCount=0
+			for ((i=$count;$i<=$maxDays;i++))
+			do
+				if [[ "${Day[$i]}" == "won" ]]
+				then
+					((luckyCount++))
+				else
+					break
+				fi
+			done
+
+			if (( $luckyCount>$luckyTemp ))
+			then
+				luckyTemp=$luckyCount
+				luckyDay=$(($i-1))
+			fi
 		fi
+
+		#####  finding unlucky Day ####
+		if [[ "${Day[$count]}" == "loose" ]]
+	   then
+   	   unluckyCount=0
+    	  for ((j=$count;$j<=$maxDays;j++))
+      	do
+         	if [[ "${Day[$j]}" == "loose" ]]
+         	then
+            	((unluckyCount++))
+ 	        else
+   	         break
+   	      fi
+   	   done
+
+   	   if (( $unluckyCount>$unluckyTemp ))
+     	 then
+     	    unluckyTemp=$unluckyCount
+     	    unluckyDay=$(($j-1))
+     	 fi
+   	fi
+
+		((count++))
+	done
+
+	echo "Most lucky day was  : day $luckyDay"
+	echo "Most unlucky day was: day $unluckyDay"
+	echo " "
+	echo "select 1 or 2: "
+	echo "1.Continue playing next month"
+	echo "2.Stop playing"
+	read choice
+
+	if (( choice==2))
+	then
+		break
+	else
+ 	echo "Gambler is playing next month also"
 	fi
-
-	#####  finding unlucky Day
-	if [[ "${Day[$count]}" == "loose" ]]
-   then
-      unluckyCount=0
-      for ((j=$count;$j<=$maxDays;j++))
-      do
-         if [[ "${Day[$j]}" == "loose" ]]
-         then
-            ((unluckyCount++))
-         else
-            break
-         fi
-      done
-
-      if (( $unluckyCount>$unluckyTemp ))
-      then
-         unluckyTemp=$unluckyCount
-         unluckyDay=$(($j-1))
-      fi
-   fi
-
-((count++))
 done
 
-echo "most lucky day was  : $luckyDay"
-echo "most unlucky day was: $unluckyDay"
+
