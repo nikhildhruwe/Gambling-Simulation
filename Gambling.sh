0ca4d0f257943d17f89755757fcd391d195d4a00
unknown <nick.dhruwe@gmail.com>
2020-06-09 03:58:30 +0530

    added days won/lost

--- Gambling.sh
+++ Gambling.sh
@@ -5,39 +5,44 @@ win=1
 loose=0
 stakePerDay=100
 maxDays=30
-
+totalAmount=0
+winCount=0
+looseCount=0
 for (( day=1;$day<=$maxDays;day++ ))
 do
-	stake=$stakePerDay
-	while (( 1 ))
-	do
-		if (($((RANDOM%2))==$win))
-		then
-			stake=$(($stake + $betMoney))
-			#echo "Won the bet"
-		else
-			stake=$(($stake - $betMoney))
-			#echo "Lost the bet"
-		fi
+   stake=$stakePerDay
+   while (( 1 ))
+   do
+      if (($((RANDOM%2))==$win))
+      then
+         stake=$(($stake + $betMoney))
+      else
+         stake=$(($stake - $betMoney))
+      fi
 
-		if (( $stake==$(($stakePerDay*50/100)) || $stake==$(($stakePerDay +$stakePerDay*50/100)) ))
-		then
-			#echo "Resigning for the day"
-			break;
-		fi
-	done
-
-  totalAmount=$(( $totalAmount + $stake))
-
-   if (( $stake>$stakePerDay ))
+      if (( $stake==$(($stakePerDay*50/100)) || $stake==$(($stakePerDay +$stakePerDay*50/100)) ))
+      then
+         totalAmount=$(($totalAmount+$stake))
+         break;
+      fi
+   done
+   if (($stake>$stakePerDay))
    then
-      difference=$(($stake-$stakePerDay))
-      echo "day $day ,amount won:    $difference"
-      echo "         ,total amount : $totalAmount"
-   else
-      difference=$(($stakePerDay-$stake))
-      echo "day $day ,amount lost:    $difference"
-      echo "         ,total amount : $totalAmount"
+		won[$day]=$day
+	else
+		lost[$day]=$day
    fi
 done
+echo "total Amount: $totalAmount "
+totalStake=$(($stakePerDay*$maxDays))
+if (( $totalAmount>$totalStake ))
+then
+    difference=$(($totalAmount-$totalStake))
+    echo "Total Amount won : $difference"
+else
+    difference=$(($totalStake-$totalAmount))
+    echo "Total Amount lost: $difference"
+fi
 
+echo "Days won : ${won[@]}"
+echo "Days lost: ${lost[@]}"
